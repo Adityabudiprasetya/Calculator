78ea99d1fe4289b16b5a648cc50103447502b882
Adityabudiprasetya <adityabudiprasetya9@gmail.com>
2023-03-13 16:56:58 +0700

    melengkapi fungsi main

--- calculator.cs
+++ calculator.cs
@@ -3,7 +3,14 @@ class Calculator
 {
 	static void main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		console.writeline("hasil penambahan: (0) + (1) = (2)", a, b, penambahan(a, b));
+		console.writeline("hasil pengurangan: (0) - (1) = (2)", a, b, pengurangan(a, b));
+		
+		console.writeline("\nTekan sembarang key untuk keluar");
+		console.Readkey();
 	}
 	static int penambahan(int a, int b)
 	{
